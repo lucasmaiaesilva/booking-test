cb49abba021073affb6b1d9ffb7c5f1199f211d9
Lucas Silva <lucassilva@Lucass-MacBook-Air.local>
2023-12-17 12:06:52 -0300

    fix: props unused issue on calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -53,8 +53,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="w-4 h-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="w-4 h-4" />,
+        IconLeft: ({ ...props }) => (
+          <ChevronLeft className="w-4 h-4" {...props} />
+        ),
+        IconRight: ({ ...props }) => (
+          <ChevronRight className="w-4 h-4" {...props} />
+        ),
       }}
       {...props}
     />
