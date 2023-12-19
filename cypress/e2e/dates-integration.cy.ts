509ad4d911ceb178bdf6784ce2825c3b59891e83
Lucas Silva <lucassilva@Lucass-MacBook-Air.local>
2023-12-19 14:37:35 -0300

    feat: add integration test end date between dates existing

--- cypress/e2e/dates-integration.cy.ts
+++ cypress/e2e/dates-integration.cy.ts
@@ -14,7 +14,7 @@ describe("Dates Integration", () => {
   });
 
   it("Fail-Integration between dates ( start date > end-date )-Mobile", () => {
-    cy.viewport("iphone-8");
+    cy.viewport("iphone-x");
     cy.visit("/");
 
     cy.selectPlace(3);
@@ -41,13 +41,39 @@ describe("Dates Integration", () => {
   });
 
   it("Fail-Integration between dates ( conflicted date - start date between existing dates )-Mobile", () => {
-    cy.viewport("macbook-16");
+    cy.viewport("iphone-x");
     cy.visit("/");
 
     cy.createBooking(2, 10, 20);
     cy.selectPlace(3);
     cy.selectDate("start-date", 11);
     cy.selectDate("end-date", 30);
+    cy.get('[aria-label="Submit Booking Info"]').click();
+
+    cy.get('[aria-label="Conflicted Dates"]').should("exist");
+  });
+
+  it("- [ ] Fail-Integration between dates ( conflicted date - end date between existing dates )-Desktop", () => {
+    cy.viewport("macbook-16");
+    cy.visit("/");
+
+    cy.createBooking(2, 10, 20);
+    cy.selectPlace(3);
+    cy.selectDate("start-date", 8);
+    cy.selectDate("end-date", 15);
+    cy.get('[aria-label="Submit Booking Info"]').click();
+
+    cy.get('[aria-label="Conflicted Dates"]').should("exist");
+  });
+
+  it("- [ ] Fail-Integration between dates ( conflicted date - end date between existing dates )-Mobile", () => {
+    cy.viewport("iphone-x");
+    cy.visit("/");
+
+    cy.createBooking(2, 10, 20);
+    cy.selectPlace(3);
+    cy.selectDate("start-date", 8);
+    cy.selectDate("end-date", 15);
     cy.get('[aria-label="Submit Booking Info"]').click();
 
     cy.get('[aria-label="Conflicted Dates"]').should("exist");
